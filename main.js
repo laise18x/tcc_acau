7fc5c3bce958f1eebbb9de925061be85bdee7d8e
Laise Xavier <laise18x@gmail.com>
2022-03-27 22:28:14 -0300

    Criação do arquivo Main.js; Implementação do contador; Implementação do efeito collapse com mudança de ícone.

---
+++ main.js
@@ -0,0 +1,23 @@
+//Contador
+
+var contadorDataFinal = new Date("Jan 5, 2028 15:37:25").getTime();
+
+var segundosUpdate = setInterval(function() {
+  var agora = new Date().getTime();
+  var diferenca = contadorDataFinal - agora;
+
+  var dias = Math.floor(diferenca / (1000 * 60 * 60 * 24));
+  var horas = Math.floor((diferenca % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutos = Math.floor((diferenca % (1000 * 60 * 60)) / (1000 * 60));
+  var segundos = Math.floor((diferenca % (1000 * 60)) / 1000);
+    
+  // Output the result in an element with id="demo"
+  document.getElementById("contador").innerHTML = dias + "d " + horas + "h "
+  + minutos + "m " + segundos + "s ";
+    
+  // If the count down is over, write some text 
+  if (diferenca < 0) {
+    clearInterval(segundosUpdate);
+    document.getElementById("contador").innerHTML = "Seu semestre já acabou!";
+  }
+}, 1000);
