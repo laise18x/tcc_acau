efdf6554c97068970cded5a57e905d8bc2d57ea3
Laise Xavier <laise18x@gmail.com>
2022-06-07 20:08:55 -0300

    Elementos da info do Fórum fixos, Criação do menu de opções das postagens com botões de editar, excluir e denunciar.

--- postagens.js
+++ postagens.js
@@ -1,22 +1,20 @@
 fetch("https://ghibliapi.herokuapp.com/films")
   .then((response) => response.json())
   .then((data) => {
-    //Aqui iremos trabalhar com o JSON
+    
     data.forEach(movie => {
-      // Criamos uma div com a classe card
+      
       const card = document.createElement('div');
       card.setAttribute('class', 'row content py-2 my-3');
     
-      // Criamos um h1 e setamos o seu textContent para o titulo do filme
       const postAuthor = document.createElement('p');
       postAuthor.setAttribute('class', 'author')
       postAuthor.textContent = movie.director;
       
-      const postTitle = document.createElement('p');
+      const postTitle = document.createElement('h3');
       postTitle.setAttribute('class', 'title')
       postTitle.textContent = movie.title;
     
-      // Criamos um elemento p e setamos o seu textContent para a descrição do filme
       const postContent = document.createElement('p');
       postContent.setAttribute('class', 'postContent')
       movie.description = movie.description.substring(0, 300); // Limitamos para 300 chars
@@ -26,14 +24,61 @@ fetch("https://ghibliapi.herokuapp.com/films")
       colName.setAttribute('class', 'col-2');
 
       const colContent = document.createElement('div');
-      colContent.setAttribute('class', 'col');
+      colContent.setAttribute('class', 'col position-relative');
+
+      const dropright = document.createElement('div');
+      dropright.setAttribute('class', 'btn-group dropend position-absolute end-0 top-0');
+
+      const kebabBtn = document.createElement('button');
+      kebabBtn.setAttribute('type', 'button');
+      kebabBtn.setAttribute('class', 'btn ');
+      kebabBtn.setAttribute('data-bs-toggle', 'dropdown');
+      kebabBtn.setAttribute('aria-expanded', 'false');
+
+      const kebabIcon = document.createElement('i');
+      kebabIcon.setAttribute('class', 'fa-solid fa-ellipsis-vertical fa-lg');
+
+      const kebabMenu = document.createElement('ul');
+      kebabMenu.setAttribute('class', 'dropdown-menu')
+
+      const listItemEdit = document.createElement('li');
+      const btnEdit = document.createElement('button');
+      btnEdit.setAttribute('class', 'dropdown-item');
+      btnEdit.setAttribute('type', 'button');
+      btnEdit.textContent = 'Editar';
+      
+      const listItemDelete = document.createElement('li');
+      const btnDelete = document.createElement('button');
+      btnDelete.setAttribute('class', 'dropdown-item');
+      btnDelete.setAttribute('type', 'button');
+      btnDelete.textContent = 'Excluir';
+
+      const listItemReport = document.createElement('li');
+      const btnReport = document.createElement('button');
+      btnReport.setAttribute('class', 'dropdown-item');
+      btnReport.setAttribute('type', 'button');
+      btnReport.textContent = 'Denunciar';
 
       // Damos Append dos cartões para o elemento contêiner
       container.appendChild(card);
     
-      // Cada cartão terá seu h1 e p
+      kebabBtn.appendChild(kebabIcon);
+      dropright.appendChild(kebabBtn);
+      
+      if(movie.director == 'Hayao Miyazaki'){
+        listItemEdit.appendChild(btnEdit);
+        listItemDelete.appendChild(btnDelete);
+        kebabMenu.appendChild(listItemEdit);
+        kebabMenu.appendChild(listItemDelete);
+      } else {
+          listItemReport.appendChild(btnReport);
+          kebabMenu.appendChild(listItemReport);
+      }
+
+      dropright.appendChild(kebabMenu);
       colName.appendChild(postAuthor);
       colContent.appendChild(postTitle);
+      colContent.appendChild(dropright);
       colContent.appendChild(postContent);
       card.appendChild(colName);
       card.appendChild(colContent);
