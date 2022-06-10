a051b18b1a4b1951432dc3b4d280ff385920af28
Laise Xavier <laise18x@gmail.com>
2022-06-10 19:46:49 -0300

    Refatoração do metodo mountPost

--- post.js
+++ post.js
@@ -1,8 +1,81 @@
 
 
-let bootstrapModal;
-function mountReportModalHTML() {
+export function mountPost(post, container){
+  console.log('mountPost');
+  const movie = post;
+  const postTemplate = document.createElement("span");
+  postTemplate.innerHTML = `
+  <div class="row content py-2 my-3">
+    <div class="col-2">
+      <p class="author">
+      </p>
+    </div>
+    <div class="col position-relative">
+      <h3 class="title">
+      </h3>
+      <div class="btn-group dropend position-absolute end-0 top-0">
+        <button
+          type="button"
+          class="btn "
+          data-bs-toggle="dropdown"
+          aria-expanded="false"
+        >
+          <i class="fa-solid fa-ellipsis-vertical fa-lg"></i>
+        </button>
+        <ul class="dropdown-menu">
+        </ul>
+      </div>
+      <p class="postContent">
+      </p>
+    </div>
+  </div>;
+  `;
+  const card = postTemplate.children.item(0);
+
+  card.querySelector(".author").textContent = movie.director;
+  card.querySelector(".title").textContent = movie.title;
+  card.querySelector(".postContent").textContent = `${movie.description}...`;
+
+  const listItemEdit = card.createElement("li");
+  const btnEdit = card.createElement("button");
+  btnEdit.setAttribute("class", "dropdown-item");
+  btnEdit.setAttribute("type", "button");
+  btnEdit.textContent = "Editar";
+
+  const listItemDelete = card.createElement("li");
+  const btnDelete = card.createElement("button");
+  btnDelete.setAttribute("class", "dropdown-item");
+  btnDelete.setAttribute("type", "button");
+  btnDelete.textContent = "Excluir";
+
+  const listItemReport = card.createElement("li");
+  const btnReport = card.createElement("button");
+  btnReport.setAttribute("class", "dropdown-item");
+  btnReport.setAttribute("type", "button");
+  btnReport.textContent = "Denunciar";
+  btnReport.addEventListener("click", (event) => {
+    event.preventDefault();
+    mountReportModalHTML();
+  });
+  kebabMenu = card.querySelector(".dropdown-menu");
+
+  if (movie.director == "Hayao Miyazaki") {
+    listItemEdit.appendChild(btnEdit);
+    listItemDelete.appendChild(btnDelete);
+    kebabMenu.appendChild(listItemEdit);
+    kebabMenu.appendChild(listItemDelete);
+  } else {
+    listItemReport.appendChild(btnReport);
+    kebabMenu.appendChild(listItemReport);
+  }
+  //const card = card.querySelector(".content");
+  container.appendChild(card);
+  console.log(container)
+}
+
+export function mountReportModalHTML() {
   if (!bootstrapModal) {
+    console.log('modal denuncia')
     const modalTemplate = document.createElement("span");
     modalTemplate.innerHTML = `
       <div class="modal fade show" id="myModal" role="dialog">
@@ -84,90 +157,3 @@ function mountReportModalHTML() {
 
   bootstrapModal.show();
 }
-
-function mountPost(post, container){
-  const movie = post;
-  const postTemplate = document.createElement("span");
-  postTemplate.innerHTML = `
-  <div class="row content py-2 my-3">
-    <div class="col-2">
-      <p class="author">
-      </p>
-    </div>
-    <div class="col position-relative">
-      <h3 class="title">
-      </h3>
-      <div class="btn-group dropend position-absolute end-0 top-0">
-        <button
-          type="button"
-          class="btn "
-          data-bs-toggle="dropdown"
-          aria-expanded="false"
-        >
-          <i class="fa-solid fa-ellipsis-vertical fa-lg"></i>
-        </button>
-        <ul class="dropdown-menu">
-        </ul>
-      </div>
-      <p class="postContent">
-      </p>
-    </div>
-  </div>;
-  `;
-  const card = postTemplate.children.item(0);
-  card.querySelector(".author").textContent = movie.director;
-  card.querySelector(".title").textContent = movie.title;
-  card.querySelector(".postContent").textContent = `${movie.description}...`;
-
-  const listItemEdit = document.createElement("li");
-  const btnEdit = document.createElement("button");
-  btnEdit.setAttribute("class", "dropdown-item");
-  btnEdit.setAttribute("type", "button");
-  btnEdit.textContent = "Editar";
-
-  const listItemDelete = document.createElement("li");
-  const btnDelete = document.createElement("button");
-  btnDelete.setAttribute("class", "dropdown-item");
-  btnDelete.setAttribute("type", "button");
-  btnDelete.textContent = "Excluir";
-
-  const listItemReport = document.createElement("li");
-  const btnReport = document.createElement("button");
-  btnReport.setAttribute("class", "dropdown-item");
-  btnReport.setAttribute("type", "button");
-  btnReport.textContent = "Denunciar";
-  btnReport.addEventListener("click", (event) => {
-    event.preventDefault();
-    mountReportModalHTML();
-  });
-  const kebabMenu = card.querySelector(".dropdown-menu");
-
-  if (movie.director == "Hayao Miyazaki") {
-    listItemEdit.appendChild(btnEdit);
-    listItemDelete.appendChild(btnDelete);
-    kebabMenu.appendChild(listItemEdit);
-    kebabMenu.appendChild(listItemDelete);
-  } else {
-    listItemReport.appendChild(btnReport);
-    kebabMenu.appendChild(listItemReport);
-  }
-  //const card = card.querySelector(".content");
-  container.appendChild(card);
-}
-
-const app = document.getElementById("postagens");
-
-const container = document.createElement("div");
-container.setAttribute("class", "container px-0");
-app.appendChild(container);
-
-fetch("https://ghibliapi.herokuapp.com/films")
-  .then((response) => response.json())
-  .then((posts) => posts.forEach(
-    (post) => mountPost(post, container)
-    ))
-  .catch((err) => {
-    const errorMessage = document.createElement("p");
-    errorMessage.textContent = `Essa não! Não está funcionando!`;
-    app.appendChild(errorMessage);
-  });
