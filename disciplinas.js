ed593deeaa3aaad145ab63668b0fdb3d0df50686
Laise Xavier <laise18x@gmail.com>
2022-06-13 23:02:41 -0300

    Remoção de páginas não utilizadas; Conclusão do modal de dicas de estudo e avaliação; Inserção do modal de envio de postagem.

---
+++ disciplinas.js
@@ -0,0 +1,74 @@
+export const disciplinas = [
+  {
+    disciplina: "Lógica de Programação",
+    targetModal: "modalDisciplina1",
+  },
+  {
+    disciplina: "Fundamentos de Tecnologia da Informação",
+    targetModal: "modalDisciplina2",
+  },
+  {
+    disciplina: "Comunicação Oral e Escrita",
+    targetModal: "modalDisciplina3",
+  },
+  {
+    disciplina: "Fundamentos de Web Design",
+    targetModal: "modalDisciplina4",
+  },
+  {
+    disciplina: "Informática Aplicada",
+    targetModal: "modalDisciplina5",
+  },
+  {
+    disciplina: "Inovação e Empreendedorismo",
+    targetModal: "modalDisciplina6",
+  },
+  {
+    disciplina: "Inovação e Empreendedorismo",
+    targetModal: "modalDisciplina6",
+  },
+  {
+    disciplina: "Programação WEB",
+    targetModal: "modalDisciplina7",
+  },
+  {
+    disciplina: "Desenvolvimento de Sistemas I",
+    targetModal: "modalDisciplina8",
+  },
+  {
+    disciplina: "Interface Humano Máquina",
+    targetModal: "modalDisciplina9",
+  },
+  {
+    disciplina: "Modelagem de Sistemas",
+    targetModal: "modalDisciplina10",
+  },
+  {
+    disciplina: "Metolodogia da Pesquisa",
+    targetModal: "modalDisciplina11",
+  },
+  {
+    disciplina: "Gestão de Projetos",
+    targetModal: "modalDisciplina12",
+  },
+  {
+    disciplina: "Desenvolvimento de Sistemas para Dispositivos Móveis",
+    targetModal: "modalDisciplina13",
+  },
+  {
+    disciplina: "Desenvolvimento de Sistemas II",
+    targetModal: "modalDisciplina14",
+  },
+  {
+    disciplina: "Banco de Dados",
+    targetModal: "modalDisciplina15",
+  },
+  {
+    disciplina: "Testes de Sistemas",
+    targetModal: "modalDisciplina16",
+  },
+  {
+    disciplina: "Trabalho de Conclusão de Curso",
+    targetModal: "modalDisciplina17",
+  },
+];
--- fluxograma.js
+++ fluxograma.js
@@ -1,43 +1,25 @@
-const disciplinas = [
-  "Lógica de Programação",
-  "Fundamentos de Tecnologia da Informação",
-  "Comunicação Oral e Escrita",
-  "Fundamentos de Web Design",
-  "Informática Aplicada",
-  "Inovação e Empreendedorismo",
-  "Programação WEB",
-  "Desenvolvimento de Sistemas I",
-  "Interface Humano Máquina",
-  "Modelagem de Sistemas",
-  "Metolodogia da Pesquisa",
-  "Gestão de Projetos",
-  "Desenvolvimento de Sistemas para Dispositivos Móveis",
-  "Desenvolvimento de Sistemas II",
-  "Banco de Dados",
-  "Testes de Sistemas",
-  "Trabalho de Conclusão de Curso"
-];
+import { generateModal } from "./modal-template.js";
+import {disciplinas} from "./disciplinas.js"
 
 let bootstrapModal;
-function createModal(disciplina, container) {
+
+function createModalBtn(disciplina, container) {
   if (!bootstrapModal) {
     const modalTemplate = document.createElement("span");
-    modalTemplate.innerHTML = `
-    <!-- Button trigger modal -->
-<button type="button" class="btn btn-primary btnDisciplina" data-bs-toggle="modal">
-</button>
-  `;
-    const modalFlow = modalTemplate.children.item(0);
+    modalTemplate.innerHTML = generateModal(disciplina.targetModal);
     const btnDisciplina = modalTemplate.querySelector(".btnDisciplina");
-    btnDisciplina.textContent = disciplina;
-    container.appendChild(modalFlow);
+    btnDisciplina.textContent = disciplina.disciplina;
+
+    const disciplinaName = modalTemplate.querySelector(".disciplinaTitle");
+    disciplinaName.textContent = disciplina.disciplina;
+    container.appendChild(modalTemplate);
   }
 }
 const app = document.getElementById("containerModal");
 const container = document.createElement("div");
-container.setAttribute("class", "container px-0");
-app.appendChild(container);
+container.setAttribute("class", "container d-flex flex-wrap align-items-start justify-content-sm-evenly px-5 mw-100 py-3");
 
 disciplinas.forEach((disciplina) => {
-  createModal(disciplina,container)
+  createModalBtn(disciplina, container);
 });
+app.appendChild(container);
